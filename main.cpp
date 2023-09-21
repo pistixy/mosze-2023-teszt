6b7fcacb3d43ec5611a7e4a4e61212f3d3bc6819
pistixy <71815251+pistixy@users.noreply.github.com>
2023-09-21 11:16:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //eliras
-    std::cout << '1-100 ertekek duplazasa' //nincs ; es a ' helyett " kell
-    for (int i = 0;) //rossz loop
+    int *b = new int[N_ELEMENTS]; //eliras
+    std::cout << "1-100 ertekek duplazasa"; //nincs ; es a ' helyett " kell
+    for (int i = 0; i < N_ELEMENTS ; i++) //rossz loop
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //hibas feltetel
+    for (int i = 0; i<N_ELEMENTS; i++) //hibas feltetel
     {
-        std::cout << "Ertek:" //nincs ;
+        std::cout << "Ertek:"; //nincs ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) //, helyett ;
+    int atlag =0;
+    for (int i = 0; i < N_ELEMENTS; i++) //, helyett ;
     {
-        atlag += b[i] //nincs ;
+        atlag += b[i]; //nincs ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
